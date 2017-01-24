768ed9f0183302475a2835b14c1f64e02e86a889
Mark Tchao <mtchao@uw.edu>
2017-01-24 11:16:40 -0800

    finish lab

--- composite-dom.ts
+++ composite-dom.ts
@@ -2,3 +2,47 @@ interface IDomElement {
   print();
 }
 
+class DomElement implements IDomElement {
+  elementName : string;
+  elements : IDomElement[];
+
+  constructor(elementName : string) {
+    this.elementName = elementName;
+    this.elements = [];
+  }
+
+  add(element:IDomElement) {
+    this.elements.push(element);
+  }
+
+  print() {
+    console.log(`<${this.elementName}>`);
+    for (let element of this.elements) {
+      element.print();
+    }
+    console.log(`</${this.elementName}>`);
+  }
+}
+
+class TextNode implements IDomElement {
+  private text : string;
+  
+  constructor(text : string) {
+    this.text = text;
+  }
+
+  print() {
+    console.log(this.text);
+  }
+}
+
+let p1 = new DomElement("p");
+let p2 = new DomElement("p");
+let div = new DomElement("div");
+let html = new DomElement("html");
+p1.add(new TextNode("Hi, I am a TextNode being printed!"));
+p2.add(new TextNode("TextNode == leaf"));
+div.add(p1);
+div.add(p2);
+html.add(div);
+html.print();
